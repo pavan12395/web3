2ebe941186b7298f4762e96e50950b2e91fbc862
Pavan Kongara <pavan.kongara@nurture.farm>
2023-11-14 20:59:50 +0530

    added changes

--- simple-storage/SimpleStorage.sol
+++ simple-storage/SimpleStorage.sol
@@ -1,17 +1,16 @@
-pragma solidity ^0.8.0;
 // SPDX-License-Identifier: MIT
 
-import "./Ownable.sol";
 
+pragma solidity ^0.8.7;
 
 contract SimpleStorage {
     mapping(address => string) data;
     function storeValue(string calldata value) external {
-        require(keccak256(abi.encodePacked(value))!=keccak256(abi.encodePacked("")),"Value should not be null");
+        require(keccak256(abi.encodePacked(value))!=keccak256(abi.encodePacked("")));
         data[msg.sender] = value;
     }
     function getValue() external view returns(string memory) {
-        require(keccak256(abi.encodePacked(data[msg.sender]))!=keccak256(abi.encodePacked("")),"No data found for this wallet");
+        require(keccak256(abi.encodePacked(data[msg.sender]))!=keccak256(abi.encodePacked("")));
         return data[msg.sender];
     }
 }
--- simple-storage/deploy.js
+++ simple-storage/deploy.js
@@ -1,10 +1,49 @@
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
 async function deploy() {
-    // compile contracts with abi , binary and deploy them on the below url
-    // deploy url :- HTTP://127.0.0.1:7545
+    try {
+        // Replace "http://127.0.0.1:7545" with the correct URL of your local Ethereum node
+        const nodeUrl = "http://127.0.0.1:7545";
+        const provider = new ethers.JsonRpcProvider(nodeUrl);
+
+        // Replace the private key with the private key of your deploying account
+        const privateKey = "0xc2043a2c6415d537e12608bfea9ce0a54dd1e508821b3ed82d19a4af85e37000";
+        const wallet = new ethers.Wallet(privateKey, provider);
+
+        // Read ABI and bytecode from files
+        const abi = fs.readFileSync("./out/SimpleStorage_sol_SimpleStorage.abi", "utf8");
+        const bytecode = fs.readFileSync("./out/SimpleStorage_sol_SimpleStorage.bin", "utf8");
+
+        // Create a contract factory
+        const contractFactory = new ethers.ContractFactory(abi, bytecode, wallet);
+
+        console.log("Deploying the contract. Please wait...");
+
+        // Deploy the contract
+        const contract = await contractFactory.deploy();
+        await contract.deployTransaction().wait(1);
+        console.log("Contract deployed to address:", contract);
+        test(contract);
+        return contract; // Return the contract instance for further use if needed
+    } catch (error) {
+        console.log(error);
+        throw error; // Rethrow the error for better error propagation
+    }
 }
 
+async function deploy(contract){
+    let transactionResponse = await contract.storeValue("hello world");
+    let transactionReciept = await transactionResponse.wait(1);
+    console.log("Store Value Response : ",transactionReciept);
+    transactionResponse = await contract.getValue();
+    transactionReciept = await transactionResponse.wait(1);
+    console.log("Get Value Response : ",transactionReciept);
+}
 
-deploy.then(()=>process.exit(0)).catch((error)=>{
-    console.log(error);
-    process.exit(1);
-})
+deploy()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error("Error deploying contract:", error);
+        process.exit(1);
+    });
